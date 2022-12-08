a2edb00b33a4fd770fd80d873417f3b37a221110
azzaomer <azzaomer70@gmail.com>
2022-12-08 15:01:15 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+ * main : prints a string using printf
+ * discription : C program that prints a string followd by a new line
+ * return : retuns int 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
