1dbc81ab70dbc5fffcd0ae226754beb1afd34118
azzaomer <azzaomer70@gmail.com>
2022-12-08 14:43:55 -0800

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-/* C Programe that prints a string using puts() function 
+
+/** main :  prints a string using puts() function 
+ * discription : C program that prints exactly Programming is like building a multilingual puzzle, followed   *  by a new line
  * returns zero
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
