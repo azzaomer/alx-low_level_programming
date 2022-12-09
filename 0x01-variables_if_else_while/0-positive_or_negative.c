5bcc9c5892b5e590b1daeccb0282aaf0244d796a
azzaomer <azzaomer70@gmail.com>
2022-12-09 15:39:22 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - chechk randoly number if its negative or positive
  * Description: prints nigative or positive or zero
  * Return: zero (success)
