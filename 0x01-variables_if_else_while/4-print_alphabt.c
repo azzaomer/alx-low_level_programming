344596dbceb8a4062eb0c3b04166662c41392e63
azzaomer <azzaomer70@gmail.com>
2022-12-09 15:57:41 -0800

    print alphabt except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints alphabt in lowercase except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter < 97; letter <= 122; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		putchar(letter);
+	}
+	putchar('\n');
+}
