ec128851b3895e8ad2181d4bdaccebc05e0e82d9
azzaomer <azzaomer70@gmail.com>
2022-12-10 08:48:09 -0800

    cmbination of two digiits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints combination of two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 48; num1 <= 57; num++)
+	{
+		for (num2 = num1 + 1; num2 <= 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			if (num1 != 56 && num2 != 57)
+				putchar(' ');
+				putchar(',');
+		}
+	}
+}
