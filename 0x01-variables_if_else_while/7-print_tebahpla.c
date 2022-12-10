e183bfc348c1b60dc350be2054857fdd4e6709b8
azzaomer <azzaomer70@gmail.com>
2022-12-09 16:26:08 -0800

    print alphabt in revers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: zero
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 122; letter <= 97; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
