146ba6d53fdbde8dabc7409aebc8c506de97f1a3
azzaomer <azzaomer70@gmail.com>
2022-12-12 09:33:48 -0800

    01-qouts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unstd.h>
+
+/**
+ * main - print string whithout using printf or put
+ *
+ * Return: returns one
+ */
+
+int main(void)
+{
+	write(2, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+ 	return (1);
+
+}
+
