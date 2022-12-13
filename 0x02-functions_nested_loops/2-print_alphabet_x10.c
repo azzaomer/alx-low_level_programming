83d8a7589f93a278b01a4ac5869435bd1228a8f4
azzaomer <azzaomer70@gmail.com>
2022-12-13 15:03:42 -0800

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Return: returns 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char j = 'a';
+
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n')
+	return (0);
+}
