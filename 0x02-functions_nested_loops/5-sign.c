5b9bdb27dd8c926606cacc82bd5768aa2e0c389e
azzaomer <azzaomer70@gmail.com>
2022-12-13 16:37:15 -0800

    prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - prints the sign of a number.
+ *@n: int type arqument
+ *Return: 1 if positive and 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(n);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
