6c98d6d5b134983555d86f2ddf4b0e35313d4040
azzaomer <azzaomer70@gmail.com>
2022-12-14 12:50:24 -0800

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,19 @@
  */
 
 int print_last_digit(int n)
-{
-	return (n % 10);
+{	
+	int nv;
+
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(nv + '0');
+		return (nv);
+	}
+	else 
+	{
+		nv = n % 10;
+		_putchar(nv + '0');
+		return (nv);
+	}
 }
