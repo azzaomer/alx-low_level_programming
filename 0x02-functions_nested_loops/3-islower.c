9191c3327b0a810d2467b95cdee1540f90e3084c
azzaomer <azzaomer70@gmail.com>
2022-12-13 16:13:36 -0800

    checks for lower letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - Return 1 if letter is lower 0 otherwise
- *
- * @c: char type letter
- * Description: Check letter if its lowercase
- * Retun: retuns 1 if lower and 0 otherwise
+ *_islower - checks for lower letter character.
+ *@c: char type
+ * Return: Retuns 1 if lower and 0 otherwise
  */
 
 int _islower(char c)
