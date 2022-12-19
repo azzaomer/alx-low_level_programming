3e764bf1dc1c1a09ab5297b26603906b93f36efd
azzaomer <azzaomer70@gmail.com>
2022-12-19 03:37:51 -0800

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,21 +8,30 @@
 
 int main(void)
 {
-	unsigned long div = 2;
+	unsigned long div;
 	unsigned long MaxFac = 0;
 	unsigned long num = 612852475143;
 
-	while (num > div)
+	if (num % 2 == 0)
+	{
+		MaxFac = 2;
+		while (num % 2 == 0)
+		{
+			num = num / 2;
+		}
+	}
+	/*At this point, num will be odd*/
+	for (div =3; div <= sqrt(num); div += 2)
 	{
 		while (num % div == 0)
 		{
-			if (div > MaxFac)
-			{
-				MaxFac = div;
-			}
+			MaxFac = div;
 			num = num / div;
 		}
-		div++;
+	}
+	if (num > 2)
+	{
+		MaxFac = num;
 	}
 	printf("%lu\n", MaxFac);
 	return (0);
