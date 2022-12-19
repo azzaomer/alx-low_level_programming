d320f70ed0579ea89fee49eae537aa844080619f
azzaomer <azzaomer70@gmail.com>
2022-12-19 06:43:04 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ *reset_to_98 - takes a pointer to an int as parameter
+*@m: pointer point to integer data type
+* Returnvoid
+*/
+
+void reset_to_98(int *m)
+{
+	*m = 98;
+}
