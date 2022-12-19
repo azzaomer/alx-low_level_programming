ce895aceff65bba4fc590aa10befc98ebdefab37
azzaomer <azzaomer70@gmail.com>
2022-12-19 09:34:09 -0800

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checkthe code
+ *
+ * Return: Always0;
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
