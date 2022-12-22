cb10e5225e3567c4f705e49f95f69dcbe31572b7
azzaomer <azzaomer70@gmail.com>
2022-12-22 02:42:29 -0800

    conver string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,13 +11,12 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\n')
+	while (str[i++])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
-		i++;
 
 	}
 
