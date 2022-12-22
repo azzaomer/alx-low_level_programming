1721534e692a9ee0e67380efaa7ecb1c15c90bdb
azzaomer <azzaomer70@gmail.com>
2022-12-22 02:10:19 -0800

    reevers array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - revers an array
+ * @a: array of integers
+ * @n: number of elmentsin the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int b = 0, c;
+	int d;
+	
+	c = n - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = a[b];
+		a[b] = a[c];
+		a[c] = d;
+	}
+}
