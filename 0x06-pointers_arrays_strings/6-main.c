a27cdd993354df79c06318992174b719871d113a
azzaomer <azzaomer70@gmail.com>
2022-12-23 07:30:28 -0800

    capitalize

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,8 +8,8 @@
  */
 
 int main(void)
-{	
-	char str[] = 
+{
+	char str[] =
 	"Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
 
