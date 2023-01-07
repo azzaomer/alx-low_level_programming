5d77060dbc2634dbd51f6fe5782de06f83dfb403
azzaomer <azzaomer70@gmail.com>
2023-01-07 01:37:28 -0800

    argc argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - checks the code 
+ * main - checks the code
  * @argc: number of command line arg
- * @array of command line arg
- * REturn: Always 0
+ * @argv: array of command line arg
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
