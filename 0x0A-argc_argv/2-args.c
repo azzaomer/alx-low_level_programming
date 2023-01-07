0386443eaf46963eac515f9bd6b66a95ff9a593f
azzaomer <azzaomer70@gmail.com>
2023-01-07 01:35:13 -0800

    argc argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - checks the code 
+ * @argc: number of command line arg
+ * @array of command line arg
+ * REturn: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
