cae9ba7d8062057475bdc217e243df7ee4dd9b1b
azzaomer <azzaomer70@gmail.com>
2023-01-07 01:01:40 -0800

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,4 +8,22 @@
 
 int _atoi(char *s)
 {
+	int i = 1;
+	unsigned int number = 0;
+
+	do {
+		if (*s == '-')
+		{
+			i *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			number = number * 10 + (*s - '0');
+		}
+		else if (number > 0)
+		{
+			break;
+		}
+	} while (*s++);
+	return (number * 1);
 }
