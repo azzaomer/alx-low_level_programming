0d9239f4a93e56d46223351e7fc87a8fc4c8f4bf
azzaomer <azzaomer70@gmail.com>
2023-01-07 01:27:41 -0800

    argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
