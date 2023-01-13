ec50e72736550eba3126bc99f1c69f236052c21c
azzaomer <azzaomer70@gmail.com>
2023-01-13 14:55:02 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(size) * nmemb);
+	p = malloc(size * nmemb);
 	if  (p == NULL)
 		return (NULL);
 	for (a = 0; a < nmemb * size; a++)
