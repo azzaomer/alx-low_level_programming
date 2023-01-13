cc7e689a98b82d43302e968e477d94eb160d2731
azzaomer <azzaomer70@gmail.com>
2023-01-13 11:04:44 -0800

    more malloc free

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -23,31 +23,31 @@ void print_number(int n)
 	{
 		while (1)
 		{
-			if (n == INT_MAX)
-			{
-			_putchar('2');
-			_putchar('1');
-			_putchar('7');
-			_putchar('4');
-			_putchar('8');
-			_putchar('3');
-			_putchar('6');
-			_putchar('4');
-			_putchar('7');
-			break;
-			}
-			else if (n == INT_MIN)
-			{
-			_putchar('2');
-			_putchar('1');
-			_putchar('7');
-			_putchar('4');
-			_putchar('8');
-			_putchar('3');
-			_putchar('6');
-			_putchar('4');
-			_putchar('7');	
-			break;
+		if (n == INT_MAX)
+		{
+		_putchar('2');
+		_putchar('1');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('7');
+		break;
+		}
+		else if (n == INT_MIN)
+		{
+		_putchar('2');
+		_putchar('1');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('7');	
+		break;
 			}
 		}
 	}
