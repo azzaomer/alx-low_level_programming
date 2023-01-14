d3f3342e84ae543879bcfa3c34d82d67499bcd63
azzaomer <azzaomer70@gmail.com>
2023-01-14 05:30:50 -0800

    malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,35 +14,27 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	unsigned int size;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	while (s1[i])
-		++i;
+		i++;
 	while (s2[i])
-		++j;
-	size = i + j + 1;
-	p = (char *) malloc(size * sizeof(char));
-	if (!p)
+		j++;
+	p = malloc(1 + (i * sizeof(char)) + (j * sizeof(char)));
+	if (p == NULL)
 		return (NULL);
-	if (s1)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		for (i = 0; s1[i] != '\0'; ++i)
-		{
-			p[i] = s1[i];
-		}
+		p[i] = s1[i];
 	}
-	if (s2)
+	for (j = 0; s2[j] != '\0'; i++, j++)
 	{
-		for (j = 0; s2[j] != '\0'; ++j)
-		{
-			p[i + j] = s2[j];
-		}
+		p[i] = s2[j];
 	}
-	p[i + j] = '\0';
+	p[i] = '\0';
 	return (p);
 }
 
