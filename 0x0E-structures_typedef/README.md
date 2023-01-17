2494eb7e76c3ca644ee0fc0002235c0a1021667e
azzaomer <azzaomer70@gmail.com>
2023-01-17 14:42:35 -0800

    structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+*** 0x0E-structures_typedef ***
+1. init_dog:
+	function that initialize a variable of type struct dog
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a new struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+	dog;
+#endif
