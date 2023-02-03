736b4fddb00ab43de60d1ee899e40f84b7672ad7
azzaomer <azzaomer70@gmail.com>
2023-02-03 06:40:42 -0800

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - append a node to the end
+ * @head: address to a node
+ * @str: a string to add
+ *
+ * Return: ponter to a list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	while (str[len])
+		len++;
+	new->len = len;
+	new->str = strdup(str);
+	if (!(*head))
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (*head);
+}
