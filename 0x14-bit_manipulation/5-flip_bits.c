4686fdbf4a886b9184cddcdbc28658971f3c9555
azzaomer <azzaomer70@gmail.com>
2023-02-03 14:58:31 -0800

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,5 @@
 /**
- * flip_bits - function that returns the number of bits you would need 
+ * flip_bits - function that returns the number of bits you would need
  * to flip to get from one number to another.
  * @n: starting
  * @m: target
