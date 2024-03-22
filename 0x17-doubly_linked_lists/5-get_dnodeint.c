440b33d9eb1636013768cf7319ba8491d238ea3b
azzaomer <azzaomer70@gmail.com>
2024-03-22 14:35:27 -0700

    returns the nth node of a linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,7 @@
 /**
  * get_dnodeint_at_index - function that returns the nth node of a linked list.
  * @head: Is the pointer to node structure
- * index: node index
+ * @index: node index
  * Return: the nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		if (head == NULL)
 			return (NULL);
 		head = head->next;
-		count ++;
+		count++;
 	}
 	return (head);
 }
