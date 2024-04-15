9a7bd2456c25f8b4470c0c3a7a5e21e5d90734f5
azzaomer <azzaomer70@gmail.com>
2024-04-14 21:22:29 -0700

    search for a value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,9 +4,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *new;
 
-	if (new != NULL)
-		if (strcmp(new->key, key) == 0)
-			return (new->value);
-	return NULL;
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	new = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
 
