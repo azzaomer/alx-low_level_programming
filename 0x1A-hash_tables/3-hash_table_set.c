1c1fe55127dbad12a5600315828c231cff76c3eb
azzaomer <azzaomer70@gmail.com>
2024-04-14 21:13:22 -0700

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
-		return(0);
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
