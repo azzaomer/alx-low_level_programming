e032a73986aad539f89e23d77cfbf1e2d5f7ce76
azzaomer <azzaomer70@gmail.com>
2024-04-14 21:24:46 -0700

    search for a value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,13 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ *
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
@@ -11,9 +20,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	new = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
+	while (new && strcmp(new->key, key) != 0)
+		new = new->next;
 
-	return ((node == NULL) ? NULL : node->value);
+	return ((new == NULL) ? NULL : new->value);
 }
 
